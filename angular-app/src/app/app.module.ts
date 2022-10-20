9e79eb5a743d378e9a4c5d9b645e5a3d938b399c
zettacamp-aditya-prabowo <aditya.bayu@zettacamp.pro>
2022-10-20 09:19:56 +0700

    angular master

--- angular-app/src/app/app.module.ts
+++ angular-app/src/app/app.module.ts
@@ -1,12 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HeaderModule } from './header/header.module';
-import { MainModule } from './main/main.module';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
-
-
 
 @NgModule({
   declarations: [
@@ -14,11 +10,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    HeaderModule,
-    MainModule
+    AppRoutingModule
   ],
-
   providers: [],
   bootstrap: [AppComponent]
 })
