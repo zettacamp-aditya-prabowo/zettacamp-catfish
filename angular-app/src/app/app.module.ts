ad3bd1e71a082a2be95cf166ff6c30b5e214de6c
aditya22081998 <123170048@student.upnyk.ac.id>
2022-10-24 16:48:19 +0700

    branch angular day5

--- angular-app/src/app/app.module.ts
+++ angular-app/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainModule } from './main/main.module';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { MainModule } from './main/main.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    MainModule
+    MainModule,
+    NoopAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- angular-app/src/app/main/input-card/input-card.component.ts
+++ angular-app/src/app/main/input-card/input-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-input-card',
@@ -6,10 +6,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./input-card.component.css']
 })
 export class InputCardComponent implements OnInit {
-
+  
+  @Output('addItem') addForm= new EventEmitter<{title: string, desc: string, status: string}>()
+  
+  addForms(items: {title: string, desc: string, status: string}){
+    this.addForm.emit({
+      title: items.title,
+      desc: items.desc,
+      status: items.status
+    })
+  }
+    
   constructor() { }
 
   ngOnInit(): void {
   }
+
 
 }
